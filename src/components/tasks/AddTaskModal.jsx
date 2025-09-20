d6bbb17fb8222ba06baf79d3379b580b22c07cbf
rakib <rakibulislamr83@gmail.com>
2025-09-20 23:21:13 +0600

    starter v2

---
+++ src/components/tasks/AddTaskModal.jsx
@@ -0,0 +1,103 @@
+import { useForm } from 'react-hook-form';
+import Modal from '../ui/Modal';
+import { useDispatch } from 'react-redux';
+import { addTask } from '../../redux/features/tasks/tasksSlice';
+
+const AddTaskModal = ({ isOpen, setIsOpen }) => {
+  const { register, handleSubmit, reset } = useForm();
+  const dispatch = useDispatch();
+
+  const onCancel = () => {
+    reset();
+    setIsOpen(false);
+  };
+
+  const onSubmit = (data) => {
+    dispatch(addTask(data));
+    onCancel();
+  };
+
+  return (
+    <Modal isOpen={isOpen} setIsOpen={setIsOpen} title="Programming Hero">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="flex flex-col mb-5">
+          <label htmlFor="title" className="mb-2">
+            Title
+          </label>
+          <input
+            className="w-full rounded-md"
+            type="text"
+            id="title"
+            {...register("title")}
+          />
+        </div>
+        <div className="flex flex-col mb-5">
+          <label htmlFor="title" className="mb-2">
+            Description
+          </label>
+          <textarea
+            className="w-full rounded-md"
+            type="text"
+            id="description"
+            {...register("description")}
+          />
+        </div>
+        <div className="flex flex-col mb-5">
+          <label htmlFor="title" className="mb-2">
+            Deadline
+          </label>
+          <input
+            className="w-full rounded-md"
+            type="date"
+            id="date"
+            {...register("date")}
+          />
+        </div>
+        <div className="flex flex-col mb-5">
+          <label htmlFor="title" className="mb-2">
+            Assign to
+          </label>
+          <select
+            className="w-full rounded-md"
+            id="assignedTo"
+            {...register("assignedTo")}
+          >
+            <option value="Rakib">Rakib</option>
+            <option value="Sakib">Sakib</option>
+            <option value="Akib">Akib</option>
+          </select>
+        </div>
+        <div className="flex flex-col mb-5">
+          <label htmlFor="title" className="mb-2">
+            Priority
+          </label>
+          <select
+            className="w-full rounded-md"
+            id="priority"
+            {...register("priority")}
+          >
+            <option defaultValue value="high">
+              High
+            </option>
+            <option value="medium">Medium</option>
+            <option value="low">Low</option>
+          </select>
+        </div>
+        <div className="flex gap-3 justify-end">
+          <button
+            onClick={() => onCancel()}
+            type="button"
+            className="btn btn-danger "
+          >
+            Cancel
+          </button>
+          <button type="submit" className="btn btn-primary ">
+            submit
+          </button>
+        </div>
+      </form>
+    </Modal>
+  );
+};
+
+export default AddTaskModal;
