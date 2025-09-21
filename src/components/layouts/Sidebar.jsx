7471d97257894a11ae074694d497f64fd0a442db
rakib <rakibulislamr83@gmail.com>
2025-09-21 21:32:09 +0600

    route

--- src/components/layouts/Sidebar.jsx
+++ src/components/layouts/Sidebar.jsx
@@ -4,22 +4,24 @@ import {
   ChatBubbleOvalLeftEllipsisIcon,
   UserCircleIcon,
   ArchiveBoxIcon,
-} from '@heroicons/react/24/solid';
+} from "@heroicons/react/24/solid";
 
-import logo from '../../assets/image/logo.png';
-import { NavLink } from 'react-router-dom';
+import logo from "../../assets/image/logo.png";
+import { NavLink } from "react-router-dom";
 
 const Sidebar = () => {
   return (
     <div className="h-screen sticky top-0 border-r-2 border-secondary/20">
       <div className="flex flex-col items-center gap-5 h-full py-5">
-        <img src={logo} alt="logo" />
+        <NavLink to="/">
+          <img src={logo} alt="logo" />
+        </NavLink>
         <NavLink
           to="/"
           className={({ isActive }) =>
             isActive
-              ? 'p-2 rounded-2xl bg-primary text-white cursor-pointer'
-              : 'p-2 rounded-2xl group hover:bg-primary text-secondary/40 cursor-pointer transition-all'
+              ? "p-2 rounded-2xl bg-primary text-white cursor-pointer"
+              : "p-2 rounded-2xl group hover:bg-primary text-secondary/40 cursor-pointer transition-all"
           }
         >
           <SquaresPlusIcon className="h-7 w-7 group-hover:text-white" />
@@ -28,8 +30,8 @@ const Sidebar = () => {
           to="/archive"
           className={({ isActive }) =>
             isActive
-              ? 'p-2 rounded-2xl bg-primary text-white cursor-pointer'
-              : 'p-2 rounded-2xl group hover:bg-primary text-secondary/40 cursor-pointer transition-all'
+              ? "p-2 rounded-2xl bg-primary text-white cursor-pointer"
+              : "p-2 rounded-2xl group hover:bg-primary text-secondary/40 cursor-pointer transition-all"
           }
         >
           <ArchiveBoxIcon className="h-7 w-7 group-hover:text-white" />
@@ -38,8 +40,8 @@ const Sidebar = () => {
           to="/chat"
           className={({ isActive }) =>
             isActive
-              ? 'p-2 rounded-2xl bg-primary text-white cursor-pointer'
-              : 'p-2 rounded-2xl group hover:bg-primary text-secondary/40 cursor-pointer transition-all'
+              ? "p-2 rounded-2xl bg-primary text-white cursor-pointer"
+              : "p-2 rounded-2xl group hover:bg-primary text-secondary/40 cursor-pointer transition-all"
           }
         >
           <ChatBubbleOvalLeftEllipsisIcon className="h-7 w-7 group-hover:text-white " />
@@ -48,8 +50,8 @@ const Sidebar = () => {
           to="/settings"
           className={({ isActive }) =>
             isActive
-              ? 'p-2 rounded-2xl bg-primary text-white cursor-pointer'
-              : 'p-2 rounded-2xl group hover:bg-primary text-secondary/40 cursor-pointer transition-all'
+              ? "p-2 rounded-2xl bg-primary text-white cursor-pointer"
+              : "p-2 rounded-2xl group hover:bg-primary text-secondary/40 cursor-pointer transition-all"
           }
         >
           <Cog6ToothIcon className="h-7 w-7 group-hover:text-white " />
@@ -58,8 +60,8 @@ const Sidebar = () => {
           to="/profile"
           className={({ isActive }) =>
             isActive
-              ? 'p-2 rounded-2xl bg-primary text-white cursor-pointer mt-auto'
-              : 'p-2 rounded-2xl group hover:bg-primary text-secondary/40 cursor-pointer transition-all  mt-auto'
+              ? "p-2 rounded-2xl bg-primary text-white cursor-pointer mt-auto"
+              : "p-2 rounded-2xl group hover:bg-primary text-secondary/40 cursor-pointer transition-all  mt-auto"
           }
         >
           <UserCircleIcon className="h-7 w-7 group-hover:text-white " />
